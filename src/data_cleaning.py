d0d796cc2066f5eed72af9073b09d9c738bdc808
randomlee101 <folorunsodaniel5@gmail.com>
2025-07-16 21:10:07 +0100

    column drop

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
 dataframe = pd.read_csv('../assets/fraudTrain.csv')
+dataframe = dataframe.drop([dataframe.columns[0], 'cc_num'], axis=1)
 
 print(dataframe.columns)
 print(dataframe.head())
-print(dataframe.shape)
+print(dataframe.duplicated().value_counts())
+print(dataframe.isna().value_counts())
+print(dataframe["is_fraud"].value_counts())
