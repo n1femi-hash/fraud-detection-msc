45bef4d78184b11932efcacf444fa293b3b82380
randomlee101 <folorunsodaniel5@gmail.com>
2025-07-31 18:15:40 +0100

    data preprocessing

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 dataframe_augmented = pd.read_csv('../assets/augmented_fraud_train.csv')
 dataframe_test_augmented = pd.read_csv('../assets/augmented_fraud_test.csv')
@@ -12,5 +13,39 @@ def data_downsampling(dataframe):
     return dataframe
 
 
+def object_encoding(data, mappings=None):
+    if mappings is None:
+        mappings = {}
+    try:
+        return mappings[data]
+    except:
+        return mappings['unknown']
+
+
+def generate_mappings(vocab):
+    mappings = {}
+    for i in range(len(vocab)):
+        mappings[vocab[i]] = i
+    mappings['unknown'] = len(mappings.values())
+    return mappings
+
+
+def data_vectorisation(dataframe):
+    columns = list(dataframe.columns)
+    for column in columns:
+        data_type = dataframe[column].dtype
+        if data_type == object:
+            vocab = np.unique(dataframe[column])
+            mappings = generate_mappings(vocab)
+            dataframe[column] = dataframe[column].apply(lambda d: object_encoding(d, mappings))
+    return dataframe
+
+
 dataframe_augmented = data_downsampling(dataframe_augmented)
 dataframe_test_augmented = data_downsampling(dataframe_test_augmented)
+
+dataframe_augmented = data_vectorisation(dataframe_augmented)
+dataframe_test_augmented = data_vectorisation(dataframe_test_augmented)
+
+dataframe_augmented.to_csv('../assets/preprocessed_fraud_train.csv', index=False)
+dataframe_test_augmented.to_csv('../assets/preprocessed_fraud_test.csv', index=False)
